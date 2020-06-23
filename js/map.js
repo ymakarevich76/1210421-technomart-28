bbef602597ccc3687be61b34663f841bb9c28ab4
JuliaKa <ymakarevich76@gmail.com>
2020-06-23 11:35:01 +0300

    Change files

--- js/map.js
+++ js/map.js
@@ -1,22 +1,22 @@
-var mapLink = document.querySelector(".map__link");
-var mapPopup = document.querySelector(".modal-map");
-var mapClose = mapPopup.querySelector(".map-close");
+var mapLink = document.querySelector('.map__link');
+var mapPopup = document.querySelector('.modal-map');
+var mapClose = mapPopup.querySelector('.map-close');
 
-mapLink.addEventListener("click", function(evt) {
+mapLink.addEventListener('click', function(evt) {
 	evt.preventDefault();
-	mapPopup.classList.add("modal-show");
+	mapPopup.classList.add('modal-show');
 });
 
-mapClose.addEventListener("click", function(evt) {
+mapClose.addEventListener('click', function(evt) {
 	evt.preventDefault();
-	mapPopup.classList.remove("modal-show");
+	mapPopup.classList.remove('modal-show');
 });
 
-window.addEventListener("keydown", function(evt) {
+window.addEventListener('keydown', function(evt) {
 	if(evt.keyCode===27) {
-		if(mapPopup.classList.contains("modal-show")) {
+		if(mapPopup.classList.contains('modal-show')) {
 			evt.preventDefault();
-			mapPopup.classList.remove("modal-show");
+			mapPopup.classList.remove('modal-show');
 		}
 	}
 });
